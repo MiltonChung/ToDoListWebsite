7da61642cd825aa4498a841ae13f1bf0e68c2655
hchung14 <56741854+hchung14@users.noreply.github.com>
2020-09-06 16:08:28 -0700

    Add Functionality

--- main.js
+++ main.js
@@ -6,14 +6,39 @@ var oldFilter = document.getElementById('old');
 var completedFilter = document.getElementById('complete');
 var incompleteFilter = document.getElementById('incomplete');
 
-
 // Form submit event
 addItem.addEventListener('submit', addToList);
-
+// Delete event
+itemList.addEventListener('click', deleteItem);
 
 
 
 function addToList(e){
   e.preventDefault();
-  console.log('hi');
+  
+  var newItem = document.getElementById('addItem').value;
+  console.log(newItem);
+
+  var li = document.createElement('li');
+  li.className = 'list-group-item mb-1';
+  li.appendChild(document.createTextNode(newItem));
+
+  var deletebtn = document.createElement('button');
+  deletebtn.className = 'btn btn-danger btn-sm float-right delete';
+  deletebtn.appendChild(document.createTextNode('x'));
+
+  li.appendChild(deletebtn);
+  itemList.appendChild(li);
+}
+
+
+function deleteItem(e){
+  if(e.target.classList.contains('delete')){
+    console.log(e.target.parentElement);
+    if(confirm(`Are you sure you want to delete ${e.target.parentElement.value}`)){
+      var li = e.target.parentElement;
+      var list = document.getElementById('items');
+      list.removeChild(li);
+    }
+  }
 }
