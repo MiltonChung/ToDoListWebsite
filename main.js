761ef6f430cc92fa57b052c360b2d0ecc87c68be
hchung14 <56741854+hchung14@users.noreply.github.com>
2020-09-07 16:08:10 -0700

    finish

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-var addItem = document.getElementById('add-container');
-var itemList = document.getElementById('itemList');
-var list = document.getElementById('items');
-var search = document.getElementById('search');
-var newFilter = document.getElementById('new');
-var oldFilter = document.getElementById('old');
-var completedFilter = document.getElementById('complete');
-var incompleteFilter = document.getElementById('incomplete');
+let addItem = document.getElementById('add-container');
+let itemList = document.getElementById('itemList');
+let list = document.getElementById('items');
+let search = document.getElementById('search');
+let newFilter = document.getElementById('new');
+let oldFilter = document.getElementById('old');
+
+let sortedByOld = false;
 
 // Form submit event
 addItem.addEventListener('submit', addToList);
@@ -13,30 +13,26 @@ addItem.addEventListener('submit', addToList);
 itemList.addEventListener('click', deleteItem);
 // Search for items
 search.addEventListener('keyup', searchItem);
-
 // Filters:
 newFilter.addEventListener('click', sortNew);
 oldFilter.addEventListener('click', sortOld);
-completedFilter.addEventListener('click', sortDone);
-incompleteFilter.addEventListener('click', sortNotDone);
-
 
 
 function addToList(e){
   e.preventDefault();
   
-  var newItem = document.getElementById('addItem').value;
+  let newItem = document.getElementById('addItem').value;
   if(newItem == ''){
     alert('Please put something');
   } else {
-    var span = document.createElement('span');
+    let span = document.createElement('span');
     span.appendChild(document.createTextNode(newItem));
 
-    var li = document.createElement('li');
+    let li = document.createElement('li');
     li.className = 'list-group-item mb-1';
     li.appendChild(span);
 
-    var deletebtn = document.createElement('button');
+    let deletebtn = document.createElement('button');
     deletebtn.className = 'btn btn-danger btn-sm float-right delete';
     deletebtn.appendChild(document.createTextNode('x'));
 
@@ -54,8 +50,7 @@ function addToList(e){
 function deleteItem(e){
   if(e.target.classList.contains('delete')){
     if(confirm(`Are you sure you want to delete ${e.target.parentNode.querySelector('span').innerHTML}?`)){
-      var li = e.target.parentElement;
-      var list = document.getElementById('items');
+      let li = e.target.parentElement;
       list.removeChild(li);
     }
   }
@@ -64,11 +59,11 @@ function deleteItem(e){
 
 
 function searchItem(e){
-  var request = e.target.value.toLowerCase();
-  var items = itemList.getElementsByTagName('li'); 
+  let request = e.target.value.toLowerCase();
+  let items = itemList.getElementsByTagName('li'); 
   
   Array.from(items).forEach(function(item){
-    var itemName = item.firstChild.textContent;
+    let itemName = item.firstChild.textContent;
     if(itemName.toLowerCase().indexOf(request) != -1){
       item.style.display = 'block';
     } else {
@@ -78,21 +73,38 @@ function searchItem(e){
 }
 
 
-function sortNew(e){
-  console.log(1)
+function sortNew(){
+  if(sortedByOld){
+    // https://stackoverflow.com/questions/12539361/how-to-reverse-the-ordering-of-list-items-in-an-unordered-list#:~:text=You%20can%20do%20it%20like,append%20it%20to%20the%20list.
+    let i = list.childNodes.length - 1;
+ 
+    while (i >= 0){
+      list.appendChild(list.childNodes[i]);
+      i = i - 1;
+    }
+    sortedByOld = false;
+    filterSelectedInd(newFilter);
+  }
 }
 
 
-function sortOld(e){
-  console.log(1)
+function sortOld(){
+  if(!sortedByOld){
+    let i = list.childNodes.length - 1;
+ 
+    while (i >= 0){
+      list.appendChild(list.childNodes[i]);
+      i = i - 1;
+    }
+    sortedByOld = true;
+    filterSelectedInd(oldFilter);
+  }
 }
 
 
-function sortDone(e){
-  console.log(1)
-}
 
-
-function sortNotDone(e){
-  console.log(1)
+function filterSelectedInd(name){
+  newFilter.className = 'filter-btn';
+  oldFilter.className = 'filter-btn';
+  name.className = 'filter-btn filter-selected';
 }
